bfc7826892aea249e1cfb300bffaca3d73d28632
Wang-Jinyong <1204255571@qq.com>
2020-03-12 07:28:47 +0800

    初始化

---
+++ src/main/java/com/example/springbootandgradleprojectdemo/SpringbootandgradleprojectdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootandgradleprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootandgradleprojectdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootandgradleprojectdemoApplication.class, args);
+	}
+
+}
